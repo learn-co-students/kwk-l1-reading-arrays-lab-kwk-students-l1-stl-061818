ee0e6bcaa856f5fea3296537e3d1abf8af3f5e7b
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-20 15:40:39 +0000

    Done.

---
+++ array.rb
@@ -0,0 +1,20 @@
+#student_one="Caroline"
+#student_two="Millie"
+#student_three="Sam"
+#student_four="Sonali"
+#puts "In this class we have a student named #{student_four}"
+
+student_list=["Caroline","Millie","Sam","Sonali"]
+#puts student_list
+
+friends=["Chandler","Joey","Monica","Ross","Rachel","Phoebe"]
+#puts friends
+
+disney=["Moana", "Merida", "Belle", "Peter Pan", "Cinderella"]
+puts disney.push("Pooh Bear", "Tigger")
+
+ages=[16, 16, 15]
+puts ages
+
+books=["Looking for Alaska", "Carry On", "The Hate U Give"] 
+puts books.push("Geometry Textbook" , "College Algebra").length
